2d83eae2bd81c341bdf0d005789e8c88fba5c9ba
Jzeff <jzeff.somera@globe.com.ph>
2023-01-13 15:10:04 +0800

    fix eth pokemon and ethnftstaker

--- src/web3/NFTPokemon/ethPokemon.js
+++ src/web3/NFTPokemon/ethPokemon.js
@@ -1,25 +1,20 @@
 import { ethers } from "ethers";
-import ABI from "./Pokemon.json";
+import ABI from './Pokemon.json'
 
-const ethPokemon = () => {
-  const CONTRACT_ADDRESS = "0x2b59174b1d25f9Ff9E84A2076a02af7815dD26c3";
+export const CONTRACT_ADDRESS ="0x2b59174b1d25f9Ff9E84A2076a02af7815dD26c3";
 
-  if (typeof window.ethereum == "undefined") {
+if (typeof window.ethereum == "undefined") {
     // No window.ethereum found, no metamask.
     // redirect to link
     // window.location.href = `https://metamask.io/`;
     console.log("No metamask installed. Intstall metamask first.");
   }
+  
+  
+const {ethereum} = window;
+const provider = new ethers.providers.Web3Provider(ethereum)
+const ethPokemon = new ethers.Contract(CONTRACT_ADDRESS, ABI.abi, provider.getSigner())
 
-  const { ethereum } = window;
-  const provider = new ethers.providers.Web3Provider(ethereum);
-  const ethPokemon = new ethers.Contract(
-    CONTRACT_ADDRESS,
-    ABI.abi,
-    provider.getSigner()
-  );
 
-  return ethPokemon;
-};
 
 export default ethPokemon;
--- src/web3/NFTStaker/ethNFTStaker.js
+++ src/web3/NFTStaker/ethNFTStaker.js
@@ -1,23 +1,24 @@
 import { ethers } from "ethers";
 import ABI from "../NFTStaker/NFTStaker.json";
 
-const ethNFTStaker = () => {
-  if (typeof window.ethereum == "undefined") {
-    console.log("No metamask installed. Intstall metamask first.");
-  }
 
-  const eth = window.ethereum;
-  const provider = new ethers.providers.Web3Provider(eth);
 
-  const CONTRACT_ADDRESS = "0xc66eEBcC0B8C38C85E0B700a3B8c03B1FeF00689";
+if (typeof window.ethereum == "undefined") {
+  // No window.ethereum found, no metamask.
+  // redirect to link
+  // window.location.href = `https://metamask.io/`;
+  console.log("No metamask installed. Intstall metamask first.");
+}
 
-  const ethNFTStaker = new ethers.Contract(
-    CONTRACT_ADDRESS,
-    ABI.abi,
-    provider.getSigner()
-  );
+const eth = window.ethereum;
+const provider = new ethers.providers.Web3Provider(eth);
 
-  return ethNFTStaker;
-};
+export const CONTRACT_ADDRESS = "0xc66eEBcC0B8C38C85E0B700a3B8c03B1FeF00689";
+
+const ethNFTStaker = new ethers.Contract(
+  CONTRACT_ADDRESS,
+  ABI.abi,
+  provider.getSigner()
+);
 
 export default ethNFTStaker;
