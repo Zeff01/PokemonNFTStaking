77a2fe03ad6a8968fabe3962d9a3effd783b4ea4
Jzeff <jzeff.somera@globe.com.ph>
2023-01-13 15:41:18 +0800

    fix ethpokemon ethnftstaker

--- src/web3/NFTPokemon/ethPokemon.js
+++ src/web3/NFTPokemon/ethPokemon.js
@@ -1,20 +1,26 @@
 import { ethers } from "ethers";
-import ABI from './Pokemon.json'
+import ABI from "./Pokemon.json";
 
-export const CONTRACT_ADDRESS ="0x2b59174b1d25f9Ff9E84A2076a02af7815dD26c3";
-
-if (typeof window.ethereum == "undefined") {
+const ethPokemon = () => {
+  if (typeof window.ethereum == "undefined") {
     // No window.ethereum found, no metamask.
     // redirect to link
     // window.location.href = `https://metamask.io/`;
-    console.log("No metamask installed. Intstall metamask first.");
+    alert("No metamask installed. Install metamask first.");
+    return;
   }
-  
-  
-const {ethereum} = window;
-const provider = new ethers.providers.Web3Provider(ethereum)
-const ethPokemon = new ethers.Contract(CONTRACT_ADDRESS, ABI.abi, provider.getSigner())
 
+  const CONTRACT_ADDRESS = "0x2b59174b1d25f9Ff9E84A2076a02af7815dD26c3";
 
+  const { ethereum } = window;
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const ethPokemon = new ethers.Contract(
+    CONTRACT_ADDRESS,
+    ABI.abi,
+    provider.getSigner()
+  );
+
+  return ethPokemon;
+};
 
 export default ethPokemon;
--- src/web3/NFTStaker/ethNFTStaker.js
+++ src/web3/NFTStaker/ethNFTStaker.js
@@ -1,24 +1,27 @@
 import { ethers } from "ethers";
 import ABI from "../NFTStaker/NFTStaker.json";
 
+const ethNFTStaker = () => {
+  if (typeof window.ethereum == "undefined") {
+    // No window.ethereum found, no metamask.
+    // redirect to link
+    // window.location.href = `https://metamask.io/`;
+    alert("No metamask installed. Install metamask first.");
+    return;
+  }
 
+  const eth = window.ethereum;
+  const provider = new ethers.providers.Web3Provider(eth);
 
-if (typeof window.ethereum == "undefined") {
-  // No window.ethereum found, no metamask.
-  // redirect to link
-  // window.location.href = `https://metamask.io/`;
-  console.log("No metamask installed. Intstall metamask first.");
-}
+  const CONTRACT_ADDRESS = "0xc66eEBcC0B8C38C85E0B700a3B8c03B1FeF00689";
 
-const eth = window.ethereum;
-const provider = new ethers.providers.Web3Provider(eth);
+  const ethNFTStaker = new ethers.Contract(
+    CONTRACT_ADDRESS,
+    ABI.abi,
+    provider.getSigner()
+  );
 
-export const CONTRACT_ADDRESS = "0xc66eEBcC0B8C38C85E0B700a3B8c03B1FeF00689";
-
-const ethNFTStaker = new ethers.Contract(
-  CONTRACT_ADDRESS,
-  ABI.abi,
-  provider.getSigner()
-);
+  return ethNFTStaker;
+};
 
 export default ethNFTStaker;
